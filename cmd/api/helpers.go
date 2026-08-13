9340af878e8de727067e0e364025ce72fa71918b
n3ro-van-Astrea <n3rooo2008@gmail.com>
2026-08-13 23:13:17 +0300

    feat: use json.MarshalIndent and envelope responses(3.4)

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -19,9 +19,11 @@ func (app *application) readIdParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+type envelope map[string]any
 
-	js, err := json.Marshal(data)
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
